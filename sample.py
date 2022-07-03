93c40fa0175ed60d40a95110ebb5e950a13a9eb1
kanugoyal <kanugoyal716@gmail.com>
2022-07-04 00:29:50 +0530

    tried adding text to image

--- sample.py
+++ sample.py
@@ -1,6 +1,9 @@
 import cv2 
 print(cv2.__version__)
 image = cv2.imread('road.jpg')
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(image,'Hello There',(0,100),font,2,(255,255,255),3) 
+ #text,coordinate,font,size of text,color,thickness of font
 print(image)
 # 1 0 -1
 cv2.imwrite('test.jpg',image)
