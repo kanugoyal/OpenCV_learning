60b1cd4b4c2daf1ee68540a5c40127e050753c89
kanugoyal <kanugoyal716@gmail.com>
2022-07-04 10:01:17 +0530

    binary operation

--- sample.py
+++ sample.py
@@ -1,12 +1,17 @@
 import cv2 
+import numpy as np
 print(cv2.__version__)
-image = cv2.imread('road.jpg')
-font = cv2.FONT_HERSHEY_COMPLEX
-cv2.putText(image,'Hello There',(0,100),font,2,(255,255,255),3) 
+image1 = cv2.imread('pictures/1bit1.png')
+image2 = cv2.imread('pictures/2bit2.png')
+
+dest_and = cv2.bitwise_and(image2,image1,mask=None)
+
+# font = cv2.FONT_HERSHEY_COMPLEX
+# cv2.putText(image,'Hello There',(0,100),font,2,(255,255,255),3) 
  #text,coordinate,font,size of text,color,thickness of font
-print(image)
+# print(image)
 # 1 0 -1
-cv2.imwrite('test.jpg',image)
-cv2.imshow('eirst image', image)
-cv2.waitKey(0)
+# cv2.imwrite('t.jpg',image)
+cv2.imshow('Bitwise And',dest_and)
+cv2.waitKey(0) 
 # cv2.destroyAllWindows() 
