9ae9f3238a8d649ff4d92d339b80ef9e972e96cd
kanugoyal <kanugoyal716@gmail.com>
2022-07-05 09:20:14 +0530

    shape detect

---
+++ shapedetect.py
@@ -0,0 +1,37 @@
+import cv2
+from cv2 import arcLength 
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(cv2.__version__)
+image = cv2.imread('pictures/shape.jpeg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+_, thrash = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    cv2.drawContours(image, [approx], 0, (0, 0, 0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1]
+    if len(approx) == 3:
+        cv2.putText(image, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+    elif len(approx) == 4:
+        x, y, w, h = cv2.boundingRect(approx)
+        aspectRatio = float(w)/h
+        print(aspectRatio)
+        if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+            cv2.putText(image, "Square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+        else:
+            cv2.putText(image, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+
+    elif len(approx) == 5:
+        cv2.putText(image, "pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+    elif len(approx) == 10: 
+        cv2.putText(image, "star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+    else:
+        cv2.putText(image, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
+cv2.imshow("shapes", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
